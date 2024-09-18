92a2cb78831962485ce671c13c79818c24eec18e
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-09-18 13:47:39 -0500

    Create model-test.php

---
+++ model-test.php
@@ -0,0 +1,17 @@
+<?php
+
+function selectTest() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT * FROM 'test'");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
