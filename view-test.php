27bdb730bbfb4a57ebfd8ff1ad8d4a396234b0a9
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-09-18 13:50:33 -0500

    Create view-test.php

---
+++ view-test.php
@@ -0,0 +1,11 @@
+<h1> Test </h1>
+
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <th>ID</th>
+      <th>value</th>
+      <th>value2</th>
+    </thead>
+  </table>
+</div>
