bc1f0b45c6cf102a61c751fef44baed88913f101
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-09-18 13:39:18 -0500

    Update util.db.php

--- util.db.php
+++ util.db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    $conn = new mysqli('mis-4013-database.mysql.database.azure.com', 'BryanY159', 'Rush-4604', 'hw3');
     
     // Check connection
     if ($conn->connect_error) {
