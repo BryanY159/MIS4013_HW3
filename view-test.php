beb4f4e2cfc663cc83e0d3d6cc25d524295fc9a2
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-09-23 13:09:43 -0500

    Update view-test.php

--- view-test.php
+++ view-test.php
@@ -3,9 +3,24 @@
 <div class="table-responsive">
   <table class="table">
     <thead>
-      <th>ID</th>
-      <th>value</th>
-      <th>value2</th>
+      <tr>
+        <th>ID</th>
+        <th>value</th>
+        <th>value2</th>
+      </tr>
     </thead>
+    <tbody>
+      <?php
+        while($test = $test->fetch_assoc()) {
+          ?>
+          <tr>
+            <td><?php echo $test['idtest'] ?></td>
+            <td><?php echo $test['value'] ?></td>
+            <td><?php echo $test['value2'] ?></td>
+          </tr>
+          <?php
+        }
+      ?>
+    </tbody>
   </table>
 </div>
