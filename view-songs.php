84ee9f53eb854bd1ff65c6d226577e649f4eba41
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-05 14:54:28 -0500

    Create view-songs.php

---
+++ view-songs.php
@@ -0,0 +1,28 @@
+<h1>Songs</h1>
+
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>SongID</th>
+        <th>Title</th>
+        <th>Genre</th>
+        <th>ArtistID</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+        while($song = $songs->fetch_assoc()) {
+          ?>
+          <tr>
+            <td><?php echo $song['SongID']; ?></td>
+            <td><?php echo $song['Title']; ?></td>
+            <td><?php echo $song['Genre']; ?></td>
+            <td><?php echo $song['ArtistID']; ?></td>
+          </tr>
+          <?php
+        }
+      ?>
+    </tbody>
+  </table>
+</div>
