49263909de3bf3839748a549a51946e2bf0b8038
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-05 17:58:37 -0500

    Update model-playlists-by-user.php

--- model-playlists-by-user.php
+++ model-playlists-by-user.php
@@ -3,7 +3,7 @@
 function selectPlaylistsByUser($UID) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select Username, PlaylistID, PlaylistName from users u join playlists p on u.UserID = p.UserID where u.UserID = ?;");
+        $stmt = $conn->prepare("select Username, Distinct PlaylistName from users u join playlists p on u.UserID = p.UserID where u.UserID = ?;");
         $stmt->bind_param("i", $UID);
         $stmt->execute();
         $result = $stmt->get_result();
