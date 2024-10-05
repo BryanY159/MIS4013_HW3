eb6fdd7d019d775cb86f645f0725dbc03c31091f
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-05 17:13:54 -0500

    Update view-playlists-with-songs.php

--- view-playlists-with-songs.php
+++ view-playlists-with-songs.php
@@ -4,13 +4,13 @@
     <?php while($playlist = $playlists->fetch_assoc()) { ?>                                                 
       <div class="card">
         <div class="card-body">
-          <h class="card-title"><?php echo $playlist['PlaylistName']; ?> /h>
+          <h class="card-title"><?php echo $playlist['PlaylistName']; ?> </h>
           <p class="card-text">
             <ul class="list-group">
               <?php
                 $songs = selectSongsByPlaylist($playlist['PlaylistName']);
                 while ($song = $songs->fetch_assoc()) { ?>
-                  <li class="list-group-item"><?php echo $song['SongID']; ?> - <?php echo $song['Title']; ?>?></li>
+                  <li class="list-group-item"><?php echo $song['SongID']; ?> - <?php echo $song['Title']; ?></li>
               <?php } ?>
             </ul>
           </p>
