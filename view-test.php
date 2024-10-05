eafb6edcf03aa3f14d2333303d5ff2e8ef1d3900
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-05 13:04:17 -0500

    Update view-test.php

--- view-test.php
+++ view-test.php
@@ -7,6 +7,7 @@
         <th>ID</th>
         <th>value</th>
         <th>value2</th>
+        <th>Hyperlink Test</th>
       </tr>
     </thead>
     <tbody>
@@ -17,6 +18,7 @@
             <td><?php echo $test_row['idtest']; ?></td>
             <td><?php echo $test_row['value']; ?></td>
             <td><?php echo $test_row['value2']; ?></td>
+            <td><a href="test2-by-test.php?id=<?php echo $test_row['idtest'];?>">Test2</a></td>
           </tr>
           <?php
         }
