475e0e296d9f8c4134e6929df95ab58439784e2e
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-05 15:42:53 -0500

    Update model-songs-by-artist.php

--- model-songs-by-artist.php
+++ model-songs-by-artist.php
@@ -3,7 +3,7 @@
 function selectSongsByArtist($AID) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select s.SongID, Title, s.Genre, a.Name as 'Artist Name' from songs s join artists a on s.ArtistID = a.ArtistID where s.ArtistID = ?;");
+        $stmt = $conn->prepare("select s.SongID, Title, s.Genre from songs s join artists a on s.ArtistID = a.ArtistID where s.ArtistID = ?;");
         $stmt->bind_param("i", $AID);
         $stmt->execute();
         $result = $stmt->get_result();
