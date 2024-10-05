7ce4174f12c827c4d20eb0dfa2c5bb1af54390b0
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-05 17:41:39 -0500

    Update model-playlists-with-songs.php

--- model-playlists-with-songs.php
+++ model-playlists-with-songs.php
@@ -17,7 +17,7 @@ function selectPlaylists() {
 function selectSongsByPlaylist($PName) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select s.SongID, Title, Genre from songs s join playlists p on s.SongID = p.SongID where PlaylistName = ?;");
+        $stmt = $conn->prepare("select s.SongID, UserID, Title, Genre from songs s join playlists p on s.SongID = p.SongID where PlaylistName = ?;");
         $stmt->bind_param("s", $PName);
         $stmt->execute();
         $result = $stmt->get_result();
