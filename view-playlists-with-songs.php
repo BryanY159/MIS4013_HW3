ed6e27fd4b62d0bad627530d9de6f9334a748717
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-05 17:06:45 -0500

    Create view-playlists-with-songs.php

---
+++ view-playlists-with-songs.php
@@ -0,0 +1,21 @@
+<h1>Playlists with Songs</h1>
+
+<div class="card-group">
+    <?php while($playlist = $playlists->fetch_assoc()) { ?>                                                 
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title"><?php echo $playlist['PlaylistName']; ?>/h5>
+          <p class="card-text">
+            <ul class="list-group">
+              <?php
+                $songs = selectSongsByPlaylist($playlist['PlaylistName']);
+                while ($song = $songs->fetch_assoc()) { ?>
+                  <li class="list-group-item"><?php echo $song['SongID']; ?> - <?php echo $song['Title']; ?> - <?php echo $song['Genre']; ?></li>
+              <?php } ?>
+            </ul>
+          </p>
+          <p class="card-text"><small class="text-body-secondary">Playlist ID: <?php echo $playlist['PlaylistID']; ?></small></p>
+        </div>
+      </div>
+    <?php } ?>
+</div>
